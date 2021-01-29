67a23f32baf145cfabf53fbc0563a1c37285156d
francois-stjacques <francois-stjacques@users.noreply.github.com>
2021-01-29 08:28:47 -0500

    Dernière modification

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const name = 'React';
+  const name = 'Chats';
   return (
     <div className="App">
       <header className="App-header">
